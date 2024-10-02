f0f3679571dad6976faaba599a9e3b49b55d9582
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-02 19:40:58 +0530

    about.js

---
+++ commands/about.js
@@ -0,0 +1,18 @@
+// commands/about.js
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'about',
+  description: 'Show about information',
+  async execute(message) {
+    const embed = new EmbedBuilder()
+      .setColor('BLUE')
+      .setTitle('About Zephyr Music Bot')
+      .setDescription('Zephyr Music Bot is a powerful music bot for Discord that supports various streaming platforms including Spotify and YouTube.')
+      .addField('Creator', 'Your Name')
+      .addField('Version', '1.0.0')
+      .addField('GitHub Repository', '[Link](https://github.com/Shafanamar/Zephyr-Music)');
+    
+    message.reply({ embeds: [embed] });
+  },
+};
