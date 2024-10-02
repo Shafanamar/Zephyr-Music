bd5bc553d5b5d06de2389f3e76124dd3a37ccba5
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-02 19:38:37 +0530

    skip.js

---
+++ commands/skip.js
@@ -0,0 +1,12 @@
+// commands/skip.js
+module.exports = {
+  name: 'skip',
+  description: 'Skip the current track',
+  async execute(message) {
+    const queue = message.client.player.getQueue(message.guild);
+    if (!queue || !queue.playing) return message.reply('No music is currently playing!');
+
+    const track = queue.skip();
+    message.reply(`⏭ Skipped **${track.title}**!`);
+  },
+};
