c7e0daadd0deae24f07654b157b51604a0c12f2a
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-02 19:40:26 +0530

    help.js

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+// commands/help.js
+const { EmbedBuilder } = require('discord.js');
+const { supportServer } = require('../config/config.json');
+
+module.exports = {
+  name: 'help',
+  description: 'Show help information',
+  async execute(message) {
+    const embed = new EmbedBuilder()
+      .setColor('PURPLE')
+      .setTitle('Help - Zephyr Music Bot')
+      .setDescription('Here are the commands you can use:')
+      .addFields(
+        { name: '!play [song]', value: 'Play a song from a URL or search query.' },
+        { name: '!stop', value: 'Stop the current song.' },
+        { name: '!skip', value: 'Skip the current song.' },
+        { name: '!queue', value: 'Show the current queue.' },
+        { name: '!about', value: 'Show information about the bot.' },
+      )
+      .addField('Support Server', supportServer);
+      
+    message.reply({ embeds: [embed] });
+  },
+};
