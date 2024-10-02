2d315deae76779d24a023626cf22148704229eee
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-02 19:51:15 +0530

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,2 +1,19 @@
-// events/ready.js
-module.exports =
+const { Events } = require('discord.js');
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Logged in as ${client.user.tag}!`);
+    
+    // Set a presence/status for the bot
+    client.user.setPresence({
+      activities: [{ name: 'music', type: 'LISTENING' }],
+      status: 'online'
+    });
+
+    // Log the number of servers the bot is in
+    console.log(`The bot is connected to ${client.guilds.cache.size} servers.`);
+  },
+};
+
