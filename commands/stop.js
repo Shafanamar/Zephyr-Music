ec00298b8fc37ed37e1552b2a84a768741bb1e96
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-02 19:38:07 +0530

    stop.js

---
+++ commands/stop.js
@@ -0,0 +1,12 @@
+// commands/stop.js
+module.exports = {
+  name: 'stop',
+  description: 'Stop the music',
+  async execute(message) {
+    const queue = message.client.player.getQueue(message.guild);
+    if (!queue || !queue.playing) return message.reply('No music is currently playing!');
+
+    queue.destroy();
+    message.reply('⏹ Stopped the music!');
+  },
+};
